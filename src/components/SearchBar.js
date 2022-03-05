fc962b699e977d49ba8f8e1c591951e1927c0eca
Gee <glynester@hotmail.com>
2022-03-05 11:03:59 +0000

    Make input controllable. onFormSubmit ready to call callback from Parent (App) to pass search term to App.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state={term:''};
+
+  onTermChange=(event)=>{
+    console.log(event.target.value);
+    this.setState({term: event.target.value})
+  }
+
+  onFormSubmit=(e)=>{
+    e.preventDefault();
+    // Call callback from parent component
+  }
 
   render(){
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input 
+              type="text" 
+              value={this.state.term}
+              // onChange={event=>this.setState({term: event.target.value})}
+              onChange={this.onTermChange}
+            />
           </div>
         </form>
       </div>
