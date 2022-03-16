f1fa779d3f403b25d9a8397271db3a727aae426d
Gee <glynester@hotmail.com>
2022-03-16 20:46:50 +0000

    Changed SearchBar component from class based to functional.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,57 @@
 import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
-  state={term:''};
+const SearchBar=({onSBFormSubmit})=>{
+  const [term, setTerm] = useState('');
 
-  onTermChange=(event)=>{
-    // console.log(event.target.value);
-    this.setState({term: event.target.value})
+  const onTermChange=(event)=>{
+    // this.setState({term: event.target.value})
+    setTerm(event.target.value);
   }
 
-  onFormSubmit=(e)=>{
+  const onFormSubmit=(e)=>{
     e.preventDefault();
     // Call callback from parent component
-    this.props.onSBFormSubmit(this.state.term);
+    // this.props.onSBFormSubmit(this.state.term);
+    onSBFormSubmit(term);
   }
 
-  render(){
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input 
-              type="text" 
-              value={this.state.term}
-              onChange={this.onTermChange}
-            />
-          </div>
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div className="search-bar ui segment">
+      {/* <form onSubmit={this.onFormSubmit} className="ui form"> */}
+      <form onSubmit={onFormSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input 
+            type="text" 
+            // value={this.state.term}
+            value={term}
+            // onChange={this.onTermChange}
+            onChange={onTermChange}
+          />
+        </div>
+      </form>
+    </div>
+  )
 }
+
+// class SearchBar extends React.Component {
+ 
+//   render(){
+//     return (
+//       <div className="search-bar ui segment">
+//         <form onSubmit={this.onFormSubmit} className="ui form">
+//           <div className="field">
+//             <label>Video Search</label>
+//             <input 
+//               type="text" 
+//               value={this.state.term}
+//               onChange={this.onTermChange}
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
 
 export default SearchBar;
