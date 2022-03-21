b179ba84bff44d0695e0eb8a409d78488286255c
Gee <glynester@hotmail.com>
2022-03-21 21:00:42 +0000

    Comments cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-// import React from 'react';
 import React, {useState, useEffect} from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
-// import youtube from '../apis/youtube';
 import {getRandom, Constants} from '../gbHelperCode/gbHelper';
 import useVideos from '../hooks/useVideos';
 
@@ -11,7 +9,6 @@ import useVideos from '../hooks/useVideos';
 const App=()=>{
   const [selectedVideo, setSelectedVideo]=useState(null);  
     const term = getRandom(Constants.SEARCH_WORDS,1)[0];
-    // Call custom hook in the same way you would call any other hook.
     const [videos, search] = useVideos(term);
 
     useEffect(()=>{
@@ -20,7 +17,6 @@ const App=()=>{
 
     return (
       <div className="ui container">
-        {/* <SearchBar onSBFormSubmit={ onTermSubmit }/> */}
         <SearchBar onSBFormSubmit={ search }/>
         <div className="ui grid">
           <div className="ui row">
@@ -30,8 +26,6 @@ const App=()=>{
             <div className="five wide column">
               <VideoList 
                 onVideoSelected={setSelectedVideo} 
-                // Equivalent
-                // onVideoSelected={(video)=>setSelectedVideo(video)} 
                 videos={videos}
               />
             </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,29 +3,19 @@ import React, { useState } from 'react';
 const SearchBar=({onSBFormSubmit})=>{
   const [term, setTerm] = useState('');
 
-  // const onTermChange=(event)=>{
-  //   // this.setState({term: event.target.value})
-  //   setTerm(event.target.value);
-  // }
-
   const onFormSubmit=(e)=>{
     e.preventDefault();
-    // Call callback from parent component
-    // this.props.onSBFormSubmit(this.state.term);
     onSBFormSubmit(term);
   }
 
   return (
     <div className="search-bar ui segment">
-      {/* <form onSubmit={this.onFormSubmit} className="ui form"> */}
       <form onSubmit={onFormSubmit} className="ui form">
         <div className="field">
-          <label>Video Search</label>
+          <label>Search for a Video</label>
           <input 
             type="text" 
-            // value={this.state.term}
             value={term}
-            // onChange={this.onTermChange}
             onChange={(event)=>setTerm(event.target.value)}
           />
         </div>
@@ -33,25 +23,5 @@ const SearchBar=({onSBFormSubmit})=>{
     </div>
   )
 }
-
-// class SearchBar extends React.Component {
- 
-//   render(){
-//     return (
-//       <div className="search-bar ui segment">
-//         <form onSubmit={this.onFormSubmit} className="ui form">
-//           <div className="field">
-//             <label>Video Search</label>
-//             <input 
-//               type="text" 
-//               value={this.state.term}
-//               onChange={this.onTermChange}
-//             />
-//           </div>
-//         </form>
-//       </div>
-//     )
-//   }
-// }
 
 export default SearchBar;
